be940505f19a35c109027a4eb4506735b2473e6d
Akshay Tilekar <akshaytilekar7@gmail.com>
2020-12-22 19:27:00 +0530

    added services : ItemServices getItems [SkuId and Price]

---
+++ PromotionEngine/Services/IItemServices.cs
@@ -0,0 +1,11 @@
+﻿using PromotionEngine.Models;
+using System.Collections.Generic;
+
+namespace PromotionEngine.Services
+{
+    public interface IItemServices
+    {
+        List<Item> GetItems();
+        Item GetItemBySkuId(char skuId);
+    }
+}
---
+++ PromotionEngine/Services/ItemServices.cs
@@ -0,0 +1,26 @@
+﻿using PromotionEngine.Helpers;
+using PromotionEngine.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PromotionEngine.Services
+{
+    public class ItemServices : IItemServices
+    {
+        public Item GetItemBySkuId(char skuId)
+        {
+            return GetItems().FirstOrDefault(x => x.SkuId == skuId);
+        }
+
+        public List<Item> GetItems()
+        {
+            return new List<Item>()
+            {
+                new Item() {SkuId = Constants.A, Name = "A", Price = 50},
+                new Item() {SkuId = Constants.B, Name = "B", Price = 30},
+                new Item() {SkuId = Constants.C, Name = "C", Price = 20},
+                new Item() {SkuId = Constants.D, Name = "D", Price = 15}
+            };
+        }
+    }
+}
