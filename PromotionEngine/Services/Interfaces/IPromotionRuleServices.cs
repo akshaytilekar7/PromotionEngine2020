8720ec0082e35e0325077f63f8cb37b843eb1f06
Akshay Tilekar <akshaytilekar7@gmail.com>
2020-12-22 19:45:02 +0530

    Added : PromotionEngine service

---
+++ PromotionEngine/Services/Interfaces/IPromotionRuleServices.cs
@@ -0,0 +1,11 @@
+﻿using PromotionEngine.Models;
+using System.Collections.Generic;
+
+namespace PromotionEngine.Services.Interfaces
+{
+    public interface IPromotionRuleServices
+    {
+        List<PromotionRule> GetPromotionRules();
+
+    }
+}
---
+++ PromotionEngine/Services/PromotionServices.cs
@@ -0,0 +1,43 @@
+﻿using PromotionEngine.Helpers;
+using PromotionEngine.Models;
+using PromotionEngine.Services.Interfaces;
+using System.Collections.Generic;
+
+namespace PromotionEngine.Services
+{
+    public class PromotionServices : IPromotionRuleServices
+    {
+        public List<PromotionRule> GetPromotionRules()
+        {
+            List<PromotionRule> promotionRules = new List<PromotionRule>()
+            {
+                new PromotionRule
+                {
+                    RuleName = "3_A_For_130 ",
+                    SkuId = Constants.A,
+                    NumberOfAppearance = 3 ,
+                    LumpSumAmountToReduceFromPrice = 20,
+                    PercentageToReduceFromPrice = 0
+                },
+                new PromotionRule
+                {
+                    RuleName = "2_B_For_45",
+                    SkuId = Constants.B,
+                    NumberOfAppearance = 2 ,
+                    LumpSumAmountToReduceFromPrice = 15,
+                    PercentageToReduceFromPrice = 0
+                },
+                new PromotionRule
+                {
+                    RuleName = "C_&_D_For_30",
+                    SkuId = Constants.C,
+                    NumberOfAppearance = 1,
+                    LumpSumAmountToReduceFromPrice = 5,
+                    PercentageToReduceFromPrice = 0,
+                    ListOfAnotherItemsToBeConsidered = new List<char>() { Constants.D }
+                },
+            };
+            return promotionRules;
+        }
+    }
+}
