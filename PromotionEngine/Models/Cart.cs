7784f9bc1582e9d70f6676fa591c39c78a5af876
Akshay Tilekar <akshaytilekar7@gmail.com>
2020-12-22 19:20:03 +0530

    added Models

---
+++ PromotionEngine/Models/Cart.cs
@@ -0,0 +1,14 @@
+﻿namespace PromotionEngine.Models
+{
+    public class Cart
+    {
+        public Cart(char skuId, int totalCount)
+        {
+            SkuId = skuId;
+            TotalCount = totalCount;
+        }
+
+        public char SkuId { get; set; }
+        public int TotalCount { get; set; }
+    }
+}
---
+++ PromotionEngine/Models/Item.cs
@@ -0,0 +1,9 @@
+﻿namespace PromotionEngine.Models
+{
+    public class Item
+    {
+        public char SkuId { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+}
