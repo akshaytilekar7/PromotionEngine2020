649dc85052677fc409efd160193a98f99947a0ad
Akshay Tilekar <akshaytilekar7@gmail.com>
2020-12-22 19:39:47 +0530

    added : PromotionRule Model

---
+++ PromotionEngine/Models/PromotionRule.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace PromotionEngine.Models
+{
+    public class PromotionRule
+    {
+        public PromotionRule()
+        {
+            ListOfAnotherItemsToBeConsidered = new List<char>();
+        }
+
+        public char SkuId { get; set; }
+
+        public string RuleName { get; set; }
+
+        public int NumberOfAppearance { get; set; }
+
+        public int PercentageToReduceFromPrice { get; set; }
+
+        public int LumpSumAmountToReduceFromPrice { get; set; }
+
+        public List<char> ListOfAnotherItemsToBeConsidered { get; set; }
+    }
+}
